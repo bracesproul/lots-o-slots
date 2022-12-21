ee11635ba5f450c077ecf354f94d484abd0d6c2d
Brace Sproul <braceasproul@gmail.com>
2022-12-21 17:19:30 -0500

    fix: build errors

--- api/src/resolvers/EmailLog/EmailLogResolver.ts
+++ api/src/resolvers/EmailLog/EmailLogResolver.ts
@@ -1,33 +1,19 @@
 import { EmailLogRepository } from '@/repositories';
 import { EmailLog } from '@/entities';
 import { Arg, Query, Resolver, Mutation } from 'type-graphql';
-import {
-  getManager,
-  getCustomRepository,
-  Transaction,
-  TransactionManager,
-  SelectQueryBuilder,
-} from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 import { CreateEmailLogInput, GetRecentEmailLogUpdate } from './types';
 
 // @Resolver(Repo)
 @Resolver()
 export class EmailLogResolver {
-  
   @Mutation(() => EmailLog, { nullable: false })
   async createEmailLog(
-    @Arg('input', { nullable: false }) input: CreateEmailLogInput,
-    @TransactionManager() manager = getManager()
+    @Arg('input', { nullable: false }) input: CreateEmailLogInput
   ): Promise<EmailLog> {
-    return getCustomRepository(EmailLogRepository).create(
-      {
-        emailId: input.emailId,
-      },
-      manager
-    );
+    return getCustomRepository(EmailLogRepository).create(input.emailId);
   }
 
-  
   @Query(() => GetRecentEmailLogUpdate, { nullable: false })
   async getRecentUpdate(): Promise<GetRecentEmailLogUpdate> {
     const { createdAt } = await getCustomRepository(
