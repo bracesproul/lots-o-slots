d3e8c245deea916cd1611f6f29853e643f7295e9
Brace Sproul <braceasproul@gmail.com>
2023-01-02 12:17:52 -0800

    fix: footer

--- web/src/features/footer/Footer.tsx
+++ web/src/features/footer/Footer.tsx
@@ -5,39 +5,48 @@ import { TwitterLogo, DiscordLogo } from '@/assets';
 const TWITTER_URL = 'https://twitter.com/lotsoslots';
 const DISCORD_URL = 'https://discord.gg/2Z8Y4Y2';
 
-export default function Footer(): ReactElement {
+export type FooterProps = {
+  showTwitter?: boolean;
+  showDiscord?: boolean;
+};
+
+export default function Footer(props: FooterProps): ReactElement {
   return (
     <div className="footer">
-      <a
-        className="footer-icons"
-        href={TWITTER_URL}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Icon
-          content={<TwitterLogo />}
-          size={'xlarge'}
-          height={26}
-          width={34}
+      {props.showTwitter && (
+        <a
           className="footer-icons"
-        />
-      </a>
+          href={TWITTER_URL}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Icon
+            content={<TwitterLogo />}
+            size={'xlarge'}
+            height={26}
+            width={34}
+            className="footer-icons"
+          />
+        </a>
+      )}
       <p className="footer-text">
         © 2022 Lots O’ Slots, LLC. All rights reserved.
       </p>
-      <a
-        className="footer-icons"
-        href={DISCORD_URL}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Icon
-          content={<DiscordLogo />}
-          size={'xlarge'}
-          height={28}
-          width={34}
-        />
-      </a>
+      {props.showDiscord && (
+        <a
+          className="footer-icons"
+          href={DISCORD_URL}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Icon
+            content={<DiscordLogo />}
+            size={'xlarge'}
+            height={28}
+            width={34}
+          />
+        </a>
+      )}
     </div>
   );
 }
