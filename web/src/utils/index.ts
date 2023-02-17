80eab54c9aff80f8937a6619e1a1eb17bc373ae6
Brace Sproul <braceasproul@gmail.com>
2023-02-16 16:59:50 -0800

    fix: select account to make default

--- web/src/utils/index.ts
+++ web/src/utils/index.ts
@@ -3,3 +3,4 @@ export { default as withAuthRequired } from './withAuthRequired';
 export { API_URL } from './apiUrl';
 export { default as handleSetUserCookie } from './handleSetUserCookie';
 export { default as findUserId } from './findUserId';
+export * from './getPaymentProviderFromString';
