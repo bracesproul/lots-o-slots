50c00bbae96a9a791d65d24b7a12bf787d924305
Brace Sproul <braceasproul@gmail.com>
2023-03-06 20:13:37 -0800

    fix: improved input component

--- web/src/components/input/Input.tsx
+++ web/src/components/input/Input.tsx
@@ -12,6 +12,12 @@ export type InputProps = {
   name?: string;
   type?: string;
   required?: boolean;
+  /** The label of the input */
+  label?: string;
+  /** Error message to display */
+  error?: string;
+  /** Whether or not the input has an error */
+  hasError?: boolean;
 };
 
 const PREFIX = StylePrefix.INPUT_COMPONENT;
@@ -19,16 +25,30 @@ const PREFIX = StylePrefix.INPUT_COMPONENT;
 export default function Input(props: InputProps): ReactElement {
   const p = { ...props };
   return (
-    <input
-      className={clsx([`${PREFIX}`, p.className])}
-      disabled={p.isDisabled}
-      name={p.name}
-      id={p.id}
-      value={p.value}
-      onChange={(e) => p.onChange(e.target.value)}
-      placeholder={p.placeholder}
-      type={p.type}
-      required={p.required}
-    />
+    <div className={clsx([`${PREFIX}`, p.className, {
+      'has-label': !!p.label,
+    }])}>
+      <span className={`${PREFIX}-label-container`}>
+        {p.label && <p className={`${PREFIX}-label`}>{p.label}</p>}
+        {p.required && <p className={clsx([`${PREFIX}-required`, {
+          'has-label': !!p.label,
+        }])}>*</p>}
+      </span>
+      <input
+        className={clsx([`${PREFIX}-input`, p.className])}
+        disabled={p.isDisabled}
+        name={p.name}
+        id={p.id}
+        value={p.value}
+        onChange={(e) => p.onChange(e.target.value)}
+        placeholder={p.placeholder}
+        type={p.type}
+        required={p.required}
+      />
+      <span>
+        {p.hasError && <p className={`${PREFIX}-error`}>{p.error}</p>}
+      </span>
+    </div>
+
   );
 }
