5911b93b2ffc6d3052c095bf40c699d9a8d9c2c7
Brace Sproul <braceasproul@gmail.com>
2023-03-07 12:18:03 -0800

    fix: deleted unused code added processing step and text

--- web/src/features/sign-up-page/SignUpPage.tsx
+++ web/src/features/sign-up-page/SignUpPage.tsx
@@ -32,113 +32,13 @@ export type SignUpPageProps = {
   className?: string;
   /** Whether or not the submit and inputs are disabled */
   isDisabled?: boolean;
+  /** State variables and setter function for controlling inputs */
   formData: SignUpFormData;
+  /** Submit handler for creating an account */
+  handleSubmit: () => void;
 };
 
 const PREFIX = StylePrefix.SIGN_UP_PAGE;
-
-function SignUpPage(props: SignUpPageProps): ReactElement {
-  const p = { ...props };
-
-  const {
-    username,
-    setUsername,
-    password,
-    setPassword,
-    email,
-    setEmail,
-    firstName,
-    setFirstName,
-    lastName,
-    setLastName,
-    } = p.formData;
-  const isSubmitDisabled = p.isDisabled || !username || !password || !firstName || !lastName;
-
-  return (
-    <div className={clsx([PREFIX, props.className])}>
-      <div className={`${PREFIX}-wrapper`}>
-        <h3 className={`${PREFIX}-title`}>Sign Up</h3>
-        <form className={`${PREFIX}-form`}>
-          <div className={`${PREFIX}-side-by-side-fields`}>
-            <Input
-              type="text"
-              required
-              value={firstName}
-              onChange={setFirstName}
-              label="First Name"
-              isDisabled={p.isDisabled}
-              className={`${PREFIX}-small-input`}
-            />
-            <Input
-              type="text"
-              required
-              value={lastName}
-              onChange={setLastName}
-              label="Last Name"
-              isDisabled={p.isDisabled}
-              className={`${PREFIX}-small-input`}
-            />
-          </div>
-          <Input
-            type="text"
-            required
-            value={username}
-            onChange={setUsername}
-            label="Username"
-            isDisabled={p.isDisabled}
-            className={`${PREFIX}-normal-input`}
-          />
-          <Input
-            type="text"
-            required
-            value={password}
-            onChange={setPassword}
-            label="Password"
-            isDisabled={p.isDisabled}
-            className={`${PREFIX}-normal-input`}
-          />
-          <Input
-            type="text"
-            required
-            value={email}
-            onChange={setEmail}
-            label="Email"
-            isDisabled={p.isDisabled}
-            className={`${PREFIX}-normal-input`}
-          />
-          <p className={`${PREFIX}-login`}><Link href={LOGIN_PAGE}>Already have an account? Login now</Link></p>
-          <Button isDisabled={isSubmitDisabled} type='submit' className={`${PREFIX}-submit-button`}>Sign Up</Button>
-        </form>
-      </div>
-    </div>
-  )
-}
-
-export default function SignUpPageContainer(): ReactElement {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const isDisabled = false;
-
-  return <SignUpPage2
-    isDisabled={isDisabled}
-    formData={{
-      username,
-      setUsername,
-      password,
-      setPassword,
-      email,
-      setEmail,
-      firstName,
-      setFirstName,
-      lastName,
-      setLastName,
-    }}
-  />;
-}
 
 enum SignUpStep {
   ENTER_INFO = 'enter-info',
@@ -164,11 +64,9 @@ function SignUpPage2(props: SignUpPageProps): ReactElement {
   const isSubmitDisabled = p.isDisabled || !username || !password || !firstName || !lastName;
 
   const handleSubmit = () => {
-    // todo implement
+    setStep(SignUpStep.PROCESSING)
+    p.handleSubmit();
   }
-
-
-
 
   return (
     <div
@@ -238,13 +136,13 @@ function SignUpPage2(props: SignUpPageProps): ReactElement {
                     className={`${PREFIX}-normal-input`}
                     labelClassName={`${PREFIX}-input-label`}
                   />
+                  <p className={`${PREFIX}-login`}><Link href={LOGIN_PAGE}>Already have an account? Login now</Link></p>
                   <div>
                     <Button
                       type="submit"
                       isDisabled={isSubmitDisabled}
                       variant="primary"
                       className={`${PREFIX}-submit-button`}
-                      labelClassName={`${PREFIX}-input-label`}
                     >
                       Submit
                     </Button>
@@ -253,8 +151,44 @@ function SignUpPage2(props: SignUpPageProps): ReactElement {
               </form>
             </div>
           )}
+          {step === SignUpStep.PROCESSING && (
+            <div className={`${PREFIX}-processing-body`}>
+              <h1>Please wait while your account is created.</h1>
+            </div>
+          )}
         </div>
       </div>
     </div>
   )
+}
+
+export default function SignUpPageContainer(): ReactElement {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isDisabled = false;
+
+  const handleSubmit = () => {
+    /** @TODO call api */
+  }
+
+  return <SignUpPage2
+    isDisabled={isDisabled}
+    handleSubmit={handleSubmit}
+    formData={{
+      username,
+      setUsername,
+      password,
+      setPassword,
+      email,
+      setEmail,
+      firstName,
+      setFirstName,
+      lastName,
+      setLastName,
+    }}
+  />;
 }
