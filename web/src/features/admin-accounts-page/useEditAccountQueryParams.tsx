293ac64d7dc95f9fa6cae5464c516675f026feb5
Brace Sproul <braceasproul@gmail.com>
2023-03-15 13:54:09 -0700

    feat: added use query params hook

---
+++ web/src/features/admin-accounts-page/useEditAccountQueryParams.tsx
@@ -0,0 +1,22 @@
+type UseEditAccountQueryParamsOptions = {
+  // add options
+};
+
+type UseEditAccountQueryParamsAction = {
+  // add actions
+};
+
+type UseEditAccountQueryParamsState = {
+  // add state
+};
+
+type UseEditAccountQueryParamsValue = UseEditAccountQueryParamsState &
+  UseEditAccountQueryParamsAction;
+
+const useEditAccountQueryParams = (
+  props?: UseEditAccountQueryParamsOptions
+): UseEditAccountQueryParamsValue => {
+  return {};
+};
+
+export default useEditAccountQueryParams;
