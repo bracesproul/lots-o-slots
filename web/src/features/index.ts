6556ddaa59bf47dd4ffb7adc4baeeec1442c01bb
Brace Sproul <braceasproul@gmail.com>
2023-03-15 11:55:45 -0700

    chore: deleted footer feature

--- web/src/features/index.ts
+++ web/src/features/index.ts
@@ -1,6 +1,4 @@
-export { PageLayout } from './page-layout';
 export { HomePage } from './home-page';
-export { Footer } from './footer';
 export { PlayCreateBox } from './play-create-box';
 export { HowToJoinTop } from './how-to-join-top';
 export { HowToJoinLower } from './how-to-join-lower';
--- web/src/features/page-layout/PageLayout.tsx
+++
@@ -1,8 +0,0 @@
-export type PageLayoutProps = {
-  children: React.ReactNode;
-};
-
-export default function PageLayout(props: PageLayoutProps): React.ReactElement {
-  const { children } = props;
-  return <div className="page-layout">{children}</div>;
-}
