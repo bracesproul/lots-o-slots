77f9e7c1e1acbc74c3005fe719590a198ed57f26
Brace Sproul <braceasproul@gmail.com>
2023-03-14 22:39:11 -0700

    feat: added form hookup

--- web/src/features/admin-page/AdminPage.tsx
+++ web/src/features/admin-page/AdminPage.tsx
@@ -1,9 +1,15 @@
-import { ReactElement } from 'react';
+import { FormEvent, ReactElement, useState } from 'react';
 import { PageChangeType, TableType } from '@/types/page-change';
 import { StylePrefix } from '@/types/style-prefix';
 import { AdminPageHeader } from '../admin-page-header';
 import { InitialFormValues, PageType, UserInfoFormData } from '@/types';
 import { Button, Input } from '@/components';
+import {
+  useGetUserDataQuery,
+  useUpdateUserDataMutation,
+  UserRole,
+} from '@/generated/graphql';
+import { useValidatePassword } from '@/hooks';
 
 export type AdminPageProps = {
   /** Whether or not the submit and inputs are disabled */
@@ -11,12 +17,15 @@ export type AdminPageProps = {
   /** State variables and setter function for controlling inputs */
   formData: UserInfoFormData;
   /** Submit handler for creating an account */
-  handleSubmit: () => void;
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
   /** Event handler for checking if a user has focused and exited an input */
   setFocusExit: (e: boolean) => void;
   /** Message to display when a password is invalid */
   invalidPasswordMessage?: string;
+  /** The initial values to populate the form with */
   initialFormValues: InitialFormValues;
+  /** Whether or not the save button is disabled. */
+  isSaveDisabled: boolean;
 };
 
 const PREFIX = StylePrefix.ADMIN_PAGE_V2;
@@ -130,5 +139,91 @@ export default function AdminPage(props: AdminPageProps): ReactElement {
 }
 
 function AdminPageContainer(): ReactElement {
-  
+  const { data, error } = useGetUserDataQuery();
+  const userData = data?.getUserBySupabaseId;
+  const initialFormValues = {
+    username: userData?.username || '',
+    password: userData?.password || '',
+    email: userData?.email || '',
+    firstName: userData?.firstName || '',
+    lastName: userData?.lastName || '',
+  };
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const { setFocusExit, invalidPasswordMessage } = useValidatePassword({
+    password,
+  });
+  const [updateUserData] = useUpdateUserDataMutation();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await updateUserData({
+      variables: {
+        input: {
+          supabaseId: userData?.supabaseId || '',
+          password: password === '' ? initialFormValues.password : password,
+          email: email === '' ? initialFormValues.email : email,
+          data: {
+            username: username === '' ? initialFormValues.username : username,
+            firstName:
+              firstName === '' ? initialFormValues.firstName : firstName,
+            lastName: lastName === '' ? initialFormValues.lastName : lastName,
+            role: UserRole.USER,
+          },
+        },
+      },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        updateUser: {
+          __typename: 'UpdatePayload',
+          success: true,
+          user: {
+            __typename: 'UserV2',
+            id: 'string',
+            firstName,
+            lastName,
+            email,
+            password,
+            username,
+            role: UserRole.USER,
+            refreshToken: 'string',
+            supabaseId: userData?.supabaseId || '',
+          },
+        },
+      },
+    });
+  };
+
+  const isSaveDisabled =
+    (!password && !email && !firstName && !lastName) ||
+    (initialFormValues.username === username &&
+      initialFormValues.password === password &&
+      initialFormValues.email === email &&
+      initialFormValues.firstName === firstName &&
+      initialFormValues.lastName === lastName);
+
+  return (
+    <AdminPage
+      formData={{
+        username,
+        setUsername,
+        password,
+        setPassword,
+        email,
+        setEmail,
+        firstName,
+        setFirstName,
+        lastName,
+        setLastName,
+      }}
+      setFocusExit={setFocusExit}
+      handleSubmit={handleSubmit}
+      invalidPasswordMessage={invalidPasswordMessage}
+      initialFormValues={initialFormValues}
+      isSaveDisabled={isSaveDisabled}
+    />
+  );
 }
