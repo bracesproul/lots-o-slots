4a6f8aac07d82025d73e77663c76fe0bb6d70785
Brace Sproul <braceasproul@gmail.com>
2023-04-05 18:34:44 -0700

    fix: revert prev change

--- api/src/services/imap/fetchEmails.ts
+++ api/src/services/imap/fetchEmails.ts
@@ -71,18 +71,15 @@ export function execute(from: string, type: EmailType) {
         f.on('message', async (msg: any, seqno: any) => {
           processMessage(msg, seqno, type);
           // Mark the email as read after parsing it
-
-          // MOVED TO INSIDE processMessage
-
-          // msg.once('attributes', function (attrs: any) {
-          //   const flags = attrs.flags.map((flag: any) => flag.toUpperCase());
-          //   if (!flags.includes('SEEN')) {
-          //     imap.addFlags(seqno, 'SEEN', function (err: any) {
-          //       if (err) throw err;
-          //       // console.log(seqno + 'Marked email as read');
-          //     });
-          //   }
-          // });
+          msg.once('attributes', function (attrs: any) {
+            const flags = attrs.flags.map((flag: any) => flag.toUpperCase());
+            if (!flags.includes('SEEN')) {
+              imap.addFlags(seqno, 'SEEN', function (err: any) {
+                if (err) throw err;
+                // console.log(seqno + 'Marked email as read');
+              });
+            }
+          });
         });
         f.once('error', function (err: any) {
           return Promise.reject(err);
@@ -170,15 +167,6 @@ function processMessage(msg: any, seqno: any, type: EmailType) {
       }
     }
     if (payload.data && payload.success) {
-      msg.once('attributes', function (attrs: any) {
-        const flags = attrs.flags.map((flag: any) => flag.toUpperCase());
-        if (!flags.includes('SEEN')) {
-          imap.addFlags(seqno, 'SEEN', function (err: any) {
-            if (err) throw err;
-          });
-        }
-      });
-
       const provider = () => {
         if (type === EmailType.PAYPAL) return PaymentProvider.PAYPAL;
         if (type === EmailType.BOFA) return PaymentProvider.ZELLE;
