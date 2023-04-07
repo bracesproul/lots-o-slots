5d6a960ceab3ac5f20d5bcf5f0949d14bdc4ed3d
Brace Sproul <braceasproul@gmail.com>
2023-04-06 19:34:50 -0700

    fix: wrapped process message in try/catch

--- api/src/services/imap/fetchEmails.ts
+++ api/src/services/imap/fetchEmails.ts
@@ -25,6 +25,7 @@ import {
   CashAppTransaction,
 } from '@/entities';
 import { stripHtml } from 'string-strip-html';
+import { DiscordLog } from '../discord';
 
 export enum EmailType {
   PAYPAL = 'PAYPAL',
@@ -51,215 +52,237 @@ async function getRecentEmail(type: EmailType) {
 }
 
 export function execute(from: string, type: EmailType) {
-  imap.openBox('INBOX', false, function (err: any, mailBox: any) {
-    if (err) {
-      console.error('error opening inbox', err);
-      imap.end();
-      return;
-    }
-    imap.search(
-      [['FROM', from], ['UNSEEN']],
-      async (err: any, results: any) => {
-        if (err) {
-          console.error(
-            'ERROR @ imap.search(). Killing connection. ERROR: ',
-            err
-          );
-          imap.end();
-        }
-
-        if (!results || !results.length) {
-          console.log('No unread mails');
-          imap.end();
-          return;
-        }
-
-        // Limit the number of emails fetched to 50
-        const fetchOptions = {
-          bodies: '',
-        };
-
-        const recentId = await getRecentEmail(type);
-
-        // const f = imap.fetch(results, fetchOptions);
-        const f = imap.fetch(results, fetchOptions);
-        f.on('message', async (msg: any, seqno: any) => {
-          processMessage(msg, seqno, type);
-          // Mark the email as read after parsing it
-          msg.once('attributes', function (attrs: any) {
-            const flags = attrs.flags.map((flag: any) => flag.toUpperCase());
-            if (!flags.includes('SEEN')) {
-              imap.addFlags(seqno, 'SEEN', function (err: any) {
-                if (err) throw err;
-                // console.log(seqno + 'Marked email as read');
-              });
-            }
-          });
-        });
-        f.once('error', function (err: any) {
-          return Promise.reject(err);
-        });
-        f.once('end', function () {
-          // console.log('Done fetching all unseen messages.');
-          imap.end();
-        });
+  try {
+    imap.openBox('INBOX', false, function (err: any, mailBox: any) {
+      if (err) {
+        console.error('error opening inbox', err);
+        imap.end();
+        return;
       }
-    );
-  });
+      imap.search(
+        [['FROM', from], ['UNSEEN']],
+        async (err: any, results: any) => {
+          if (err) {
+            console.error(
+              'ERROR @ imap.search(). Killing connection. ERROR: ',
+              err
+            );
+            imap.end();
+          }
+
+          if (!results || !results.length) {
+            console.log('No unread mails');
+            imap.end();
+            return;
+          }
+
+          // Limit the number of emails fetched to 50
+          const fetchOptions = {
+            bodies: '',
+          };
+
+          const recentId = await getRecentEmail(type);
+
+          // const f = imap.fetch(results, fetchOptions);
+          const f = imap.fetch(results, fetchOptions);
+          f.on('message', async (msg: any, seqno: any) => {
+            processMessage(msg, seqno, type);
+            // Mark the email as read after parsing it
+            msg.once('attributes', function (attrs: any) {
+              const flags = attrs.flags.map((flag: any) => flag.toUpperCase());
+              if (!flags.includes('SEEN')) {
+                imap.addFlags(seqno, 'SEEN', function (err: any) {
+                  if (err) throw err;
+                  // console.log(seqno + 'Marked email as read');
+                });
+              }
+            });
+          });
+          f.once('error', function (err: any) {
+            return Promise.reject(err);
+          });
+          f.once('end', function () {
+            // console.log('Done fetching all unseen messages.');
+            imap.end();
+          });
+        }
+      );
+    });
+  } catch (e) {
+    console.error('ERROR RUNNING EXECUTE IMAP', e);
+    const discordLog = new DiscordLog();
+    discordLog.baseError({
+      title: 'Error processing message',
+      description: JSON.stringify(e),
+      stack: 'api/src/services/imap/fetchEmails.ts:execute()',
+    });
+  }
 }
 
 function processMessage(msg: any, seqno: any, type: EmailType) {
-  let subject = '';
-  let to = '';
-  let from = '';
+  try {
+    let subject = '';
+    let to = '';
+    let from = '';
 
-  const parser = new MailParser();
-  parser.on('headers', (headers: any) => {
-    subject = headers.get('subject');
-    from = headers.get('from').value[0].address;
-    to = headers.get('to').value[0].address;
-  });
+    const parser = new MailParser();
+    parser.on('headers', (headers: any) => {
+      subject = headers.get('subject');
+      from = headers.get('from').value[0].address;
+      to = headers.get('to').value[0].address;
+    });
 
-  parser.on('data', async (data: any) => {
-    const emailId = Number(seqno);
-    let emailLog: EmailLogV2 = {} as EmailLogV2;
-    let payload: ParsedEmailPayload = {
-      success: false,
-      data: null,
-    };
+    parser.on('data', async (data: any) => {
+      const emailId = Number(seqno);
+      let emailLog: EmailLogV2 = {} as EmailLogV2;
+      let payload: ParsedEmailPayload = {
+        success: false,
+        data: null,
+      };
 
-    const previousEmailLog = await getCustomRepository(
-      EmailLogV2Repository
-    ).findByEmailId(emailId);
-    if (previousEmailLog) {
-      return;
-    }
+      const previousEmailLog = await getCustomRepository(
+        EmailLogV2Repository
+      ).findByEmailId(emailId);
+      if (previousEmailLog) {
+        return;
+      }
 
-    if (data.type === 'text') {
-      emailLog = await getCustomRepository(EmailLogV2Repository).create({
-        emailId,
-        subject,
-        body: data.text,
-        receivedAt: new Date(),
-      });
-      if (type === EmailType.PAYPAL) {
-        const payPalPayload = parsePayPalPayment(data.text);
-
-        if (payPalPayload) {
-          payload = payPalPayload;
-        }
-      } else if (type === EmailType.BOFA) {
-        const bofaPayload = parseZellePayment(data.text);
-
-        if (bofaPayload) {
-          payload = bofaPayload;
-        }
-      } else if (type === EmailType.CASHAPP_DEPOSIT) {
-        const bodyStippedHtml = stripHtml(data.html).result;
-        const cashAppPayload = await parseCashAppPayment(bodyStippedHtml);
-        await getCustomRepository(EmailLogV2Repository).update({
-          ...emailLog,
-          body: bodyStippedHtml,
+      if (data.type === 'text') {
+        emailLog = await getCustomRepository(EmailLogV2Repository).create({
+          emailId,
+          subject,
+          body: data.text,
+          receivedAt: new Date(),
         });
-        if (cashAppPayload) {
-          payload = cashAppPayload;
+        if (type === EmailType.PAYPAL) {
+          const payPalPayload = parsePayPalPayment(data.text);
 
-          // Update account balance
-          const account = await getCustomRepository(
-            AccountRepository
-          ).findCashappAccountByEmail(to);
-          if (account && !!payload.data?.amount) {
-            await getCustomRepository(AccountRepository).creditAccountBalance({
-              id: account.id,
-              amount: payload.data.amount,
-            });
+          if (payPalPayload) {
+            payload = payPalPayload;
+          }
+        } else if (type === EmailType.BOFA) {
+          const bofaPayload = parseZellePayment(data.text);
+
+          if (bofaPayload) {
+            payload = bofaPayload;
+          }
+        } else if (type === EmailType.CASHAPP_DEPOSIT) {
+          const bodyStippedHtml = stripHtml(data.html).result;
+          const cashAppPayload = await parseCashAppPayment(bodyStippedHtml);
+          await getCustomRepository(EmailLogV2Repository).update({
+            ...emailLog,
+            body: bodyStippedHtml,
+          });
+          if (cashAppPayload) {
+            payload = cashAppPayload;
+
+            // Update account balance
+            const account = await getCustomRepository(
+              AccountRepository
+            ).findCashappAccountByEmail(to);
+            if (account && !!payload.data?.amount) {
+              await getCustomRepository(AccountRepository).creditAccountBalance(
+                {
+                  id: account.id,
+                  amount: payload.data.amount,
+                }
+              );
+            }
           }
         }
       }
-    }
 
-    if (payload.data && payload.success) {
-      await getCustomRepository(EmailLogV2Repository).markAsProcessed({
-        id: emailLog.id,
-      });
-
-      const provider = () => {
-        if (type === EmailType.PAYPAL) return PaymentProvider.PAYPAL;
-        if (type === EmailType.BOFA) return PaymentProvider.ZELLE;
-        return PaymentProvider.CASHAPP;
-      };
-
-      const paymentType = () => {
-        // if (type === EmailType.CASHAPP_WITHDRAWAL)
-        //   return PaymentType.WITHDRAWAL;
-        return PaymentType.DEPOSIT;
-      };
-
-      let cashAppTransaction: CashAppTransaction | undefined = undefined;
-      let payPalTransaction: PayPalTransaction | undefined = undefined;
-      let bankOfAmericaTransaction: BankOfAmericaTransaction | undefined =
-        undefined;
-
-      const transaction = await getCustomRepository(
-        TransactionRepository
-      ).create({
-        status: PaymentStatus.PENDING,
-        amount: payload.data.amount,
-        senderName: payload.data.name,
-        emailLog,
-        emailLogId: emailLog.id,
-        provider: provider(),
-        paymentType: paymentType(),
-      });
-      if (type === EmailType.CASHAPP_DEPOSIT) {
-        cashAppTransaction = await getCustomRepository(
-          CashAppTransactionRepository
-        ).create({
-          transaction,
-          transactionId: transaction.id,
-          amount: payload.data.amount,
-          cashtag: payload.data.name,
-          cashAppId: payload.data.transactionId,
+      if (payload.data && payload.success) {
+        await getCustomRepository(EmailLogV2Repository).markAsProcessed({
+          id: emailLog.id,
         });
-      } else if (type === EmailType.PAYPAL) {
-        payPalTransaction = await getCustomRepository(
-          PayPalTransactionRepository
+
+        const provider = () => {
+          if (type === EmailType.PAYPAL) return PaymentProvider.PAYPAL;
+          if (type === EmailType.BOFA) return PaymentProvider.ZELLE;
+          return PaymentProvider.CASHAPP;
+        };
+
+        const paymentType = () => {
+          // if (type === EmailType.CASHAPP_WITHDRAWAL)
+          //   return PaymentType.WITHDRAWAL;
+          return PaymentType.DEPOSIT;
+        };
+
+        let cashAppTransaction: CashAppTransaction | undefined = undefined;
+        let payPalTransaction: PayPalTransaction | undefined = undefined;
+        let bankOfAmericaTransaction: BankOfAmericaTransaction | undefined =
+          undefined;
+
+        const transaction = await getCustomRepository(
+          TransactionRepository
         ).create({
-          transaction,
-          transactionId: transaction.id,
+          status: PaymentStatus.PENDING,
           amount: payload.data.amount,
-          senderIdentifier: payload.data.name,
-          payPalId: payload.data.transactionId,
+          senderName: payload.data.name,
+          emailLog,
+          emailLogId: emailLog.id,
+          provider: provider(),
+          paymentType: paymentType(),
         });
-      } else if (type === EmailType.BOFA) {
-        bankOfAmericaTransaction = await getCustomRepository(
-          BankOfAmericaTransactionRepository
-        ).create({
-          transaction,
-          transactionId: transaction.id,
-          amount: payload.data.amount,
-          senderIdentifier: payload.data.name,
+        if (type === EmailType.CASHAPP_DEPOSIT) {
+          cashAppTransaction = await getCustomRepository(
+            CashAppTransactionRepository
+          ).create({
+            transaction,
+            transactionId: transaction.id,
+            amount: payload.data.amount,
+            cashtag: payload.data.name,
+            cashAppId: payload.data.transactionId,
+          });
+        } else if (type === EmailType.PAYPAL) {
+          payPalTransaction = await getCustomRepository(
+            PayPalTransactionRepository
+          ).create({
+            transaction,
+            transactionId: transaction.id,
+            amount: payload.data.amount,
+            senderIdentifier: payload.data.name,
+            payPalId: payload.data.transactionId,
+          });
+        } else if (type === EmailType.BOFA) {
+          bankOfAmericaTransaction = await getCustomRepository(
+            BankOfAmericaTransactionRepository
+          ).create({
+            transaction,
+            transactionId: transaction.id,
+            amount: payload.data.amount,
+            senderIdentifier: payload.data.name,
+          });
+        }
+        await getCustomRepository(TransactionRepository).update({
+          id: transaction.id,
+          bankOfAmericaTransaction,
+          bankOfAmericaTransactionId: bankOfAmericaTransaction?.id,
+          cashAppTransaction,
+          cashAppTransactionId: cashAppTransaction?.id,
+          payPalTransaction,
+          payPalTransactionId: payPalTransaction?.id,
         });
       }
-      await getCustomRepository(TransactionRepository).update({
-        id: transaction.id,
-        bankOfAmericaTransaction,
-        bankOfAmericaTransactionId: bankOfAmericaTransaction?.id,
-        cashAppTransaction,
-        cashAppTransactionId: cashAppTransaction?.id,
-        payPalTransaction,
-        payPalTransactionId: payPalTransaction?.id,
-      });
-    }
-  });
-
-  msg.on('body', function (stream: any) {
-    stream.on('data', function (chunk: any) {
-      parser.write(chunk.toString('utf8'));
     });
-  });
-  msg.once('end', function () {
-    console.log('Finished msg #' + seqno);
-    parser.end();
-  });
+
+    msg.on('body', function (stream: any) {
+      stream.on('data', function (chunk: any) {
+        parser.write(chunk.toString('utf8'));
+      });
+    });
+    msg.once('end', function () {
+      console.log('Finished msg #' + seqno);
+      parser.end();
+    });
+  } catch (e) {
+    console.error('ERROR PROCESSING MESSAGE', e);
+    const discordLog = new DiscordLog();
+    discordLog.baseError({
+      title: 'Error processing message',
+      description: JSON.stringify(e),
+      stack: 'api/src/services/imap/fetchEmails.ts:processMessage()',
+    });
+  }
 }
